85938346ecd0850c547e7200945e675063c941f7
lundeens <Lundeens@oregonstate.edu>
2019-03-28 09:19:46 -0700

    Added geometric uncertainties

--- geo.py
+++ geo.py
@@ -7,23 +7,39 @@ import math
 # Functions
 def size(hsn):  # basic geometry : (length, spacing, width, thickness, # of fins) [m]
     if hsn == 0:
-        g_b = (0.0089, 0.0018, 0.0078, 0.0007, 180)
+        g_b = (0.0089, 0.0018, 0.0078, 0.0007, 180)                     # geometry of test heat sink
     if hsn == 1:
-        geo_fp = (ra(hsn), 0.0508, 0.00225806)  # (Ra, Lc, A) for flat plate
+        geo_fp = (ra(hsn), 0.0508, 0.00225806, 1.7145 * 10 ** -6)                          # (Ra, Lc [m], A [m^2], u_A [m^2]) for flat plate
         return geo_fp
     if 2 <= hsn <= 15:
-        g_b = (1, 0.13125, 2, .1, 7)
+        h = 1 * 0.0254                                                  # fin height (length), [m]
+        s = 0.13125 * 0.0254                                            # fin spacing, [m]
+        w = 2 * 0.0254                                                  # fin width, [m]
+        t = .1 * 0.0254                                                 # fin thickness, [m]
+        n = 7                                                           # number of fins, [unit-less]
     if hsn > 15:
         return print('heat sink number not recognized (geo.size)')
-    # calculated geometry: (perimeter, l_c, a_space, a_cross-s, a_base) [m, m^2], fixed tuple
-    g_c = (2 * g_b[2] + 2 * g_b[3], 2 * g_b[1], g_b[0] * g_b[1], g_b[2] * g_b[3], g_b[1] * g_b[2] * (g_b[4] + 1))
-    a_fin = sa(hsn)                                             # fin area, [m^2]
-    a_tot = g_b[4] * a_fin + g_c[4]                             # fin area + base area, [m^2]
-    d_h = 4 * g_c[2] / (2 * g_b[0] + g_b[1])                    # hydraulic diameter, [m] only used for f
-    sr = ra(hsn)
-    # (hydraulic diameter, surface roughness, perimeter, lc, cross-section, length, fin area, # of fins, total area)
-    geo = (d_h, sr, g_c[0], g_c[1], g_c[3], g_b[0], a_fin, g_b[4], g_c[4], a_tot)
-    return geo
+
+    u_b_st = 2.54 * 10 ** -5                                            # standard caliper bias, [m]
+    perimeter = 2 * w + 2 * t                                           # space perimeter, [m]
+    u_b_perimeter = math.sqrt(2 * (2 * u_b_st ) ** 2)                   # perimeter bias unc, [m]
+    l_c = 2 * s                                                         # characteristic length, [m]
+    u_b_l_c = 2 * u_b_st                                                # l_c bias unc, [m]
+    a_space = h * s                                                     # area of the space, [m^2]
+    u_b_a_space = math.sqrt(((s * u_b_st) ** 2) + ((h * u_b_st) ** 2))  # a_space bias unc, [m^2]
+    a_xc = w * t                                                        # fin cross-sectional area, [m^2]
+    u_b_a_xc = math.sqrt(((w * u_b_st) ** 2) + ((t * u_b_st) ** 2))     # a_xc bias unc, [m^2]
+    a_base = s * w * (n + 1)                                            # base area, [m^2]
+    u_b_a_base = math.sqrt(((w * (n + 1) * u_b_st) ** 2) + ((s * (n + 1) * u_b_st) ** 2))  # a_base bias unc, [m^2]
+    a_fin = sa(hsn)                                                     # fin area, [m^2]
+    a_tot = n * a_fin + a_base                                          # fin area + base area, [m^2]
+    u_b_a_tot = math.sqrt(((w * (n + 1) * u_b_st) ** 2) + ((s * (n + 1) * u_b_st) ** 2))  # a_tot bias unc, [m^2]
+    d_h = 4 * a_space / (2 * h + s)                                     # hydraulic diameter, [m] only used for f
+    u_b_d_h = math.sqrt((4 * u_b_a_space / (2 * h + s)) ** 2 + 2 * (4 * a_space * u_b_st / ((2 * h + s) ** 2)) ** 2)  # d_h bias unc, [m]
+    sr = ra(hsn)                                                        # surface roughness, [m]
+    geo = (d_h, sr, perimeter, l_c, a_xc, h, a_fin, n, a_base, a_tot)
+    u_geo = (u_b_d_h, u_b_perimeter, u_b_l_c, u_b_a_xc, u_b_st, u_b_a_base, u_b_a_tot)
+    return geo, u_geo
 
 
 def ra(hsn):  # returns the value of the surface roughness for a given heat sink number
@@ -40,3 +56,7 @@ def sa(hsn):  # returns the exposed surface area of each heat sink
 
 def fp():
     return ValueError('Must use flat plate correlations for HSN 1')
+
+
+if __name__ == '__main__':
+    g = size(2)
