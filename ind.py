838dae868e84a8d0ca26927287edba404d4c7384
lundeens <Lundeens@oregonstate.edu>
2019-03-31 21:15:47 -0700

    changed to single evaluations
    changed the functions to evaluate single values instead of lists.

--- ind.py
+++ ind.py
@@ -14,110 +14,68 @@ def kcu(t):
 
 
 def reynolds(v, density, diam_hyd, visc_d):  # finds the reynolds number by calling reynolds()
-    re = []
-    if type(v) == list:
-        for i in range(0, len(v)):
-            re.append(density * v[i] * diam_hyd / visc_d)
-        return re
-    else:
-        re = density * v * diam_hyd / visc_d
-        return re
+    type(v)
+    type(density)
+    type(diam_hyd)
+    type(visc_d)
+    return density * v * diam_hyd / visc_d
 
 
 def darcy(re, surf_rough, diam_hyd):  # finds the darcy friction factor
-    f = []
-    for i in range(0, len(re)):
-        guess = 0.1  # arbitrary guess to begin the calculation
-        new_guess = (1 / (-2 * math.log10((surf_rough / 2.7 * diam_hyd)) + (2.51 / re[i] * math.sqrt(guess)))) ** 2
-        while abs(guess - new_guess) > 0.001 * guess:
-            guess = new_guess
-            new_guess = (1 / (-2 * math.log10((surf_rough / (3.7 * diam_hyd)) + (2.51 / (re[i] * math.sqrt(guess)))))) ** 2
-        f.append(new_guess)
-    return f
+    guess = 0.1  # arbitrary guess to begin the calculation
+    new_guess = (1 / (-2 * math.log10((surf_rough / 2.7 * diam_hyd)) + (2.51 / re * math.sqrt(guess)))) ** 2
+    while abs(guess - new_guess) > 0.001 * guess:
+        guess = new_guess
+        new_guess = (1 / (-2 * math.log10((surf_rough / (3.7 * diam_hyd)) + (2.51 / (re * math.sqrt(guess)))))) ** 2
+    return new_guess
 
 
 def nusselt(f, re, pr):  # finds the nusselt number
-    nu = []
-    for i in range(0, len(f)):
-        nu.append((f[i] / 8) * (re[i] - 1000) * pr / (1 + 12.7 * math.sqrt(f[i] / 8) * ((pr ** (2 / 3)) - 1)))
-    return nu
+    return (f / 8) * (re - 1000) * pr / (1 + 12.7 * math.sqrt(f / 8) * ((pr ** (2 / 3)) - 1))
 
 
 def hxc(nu, k_air, diam_hyd):  # finds the convective heat transfer coefficient
-    hxc = []
-    for i in range(0, len(nu)):
-        hxc.append(nu[i] * k_air / diam_hyd)
-    return hxc
+    return nu * k_air / diam_hyd
 
 
 def little_m(h, p, k, a_c):  # finds the fin temperature profile constant
-    m = []
-    for i in range(0, len(h)):
-        m.append(math.sqrt(h[i] * p / (k * a_c)))
-    return m
+    return math.sqrt(h * p / (k * a_c))
 
 
 def big_m(h, p, k, a_c, dtb):  # finds the fin heat rate constant
-    m = []
-    for i in range(0, len(h)):
-        m.append(math.sqrt(h[i] * p * k * a_c) * dtb)
-    return m
+    return math.sqrt(h * p * k * a_c) * dtb
 
 
 def q_f(em, m, l, h, k):  # finds the fin heat rate
-    qf = []
-    for i in range(0, len(em)):
-        qf.append(em[i] * (math.sinh(m[i] * l) + (h[i] / (m[i] * k)) * math.cosh(m[i] * l)) / (math.cosh(m[i] * l) + (h[i] / (m[i] * k)) * math.sinh(m[i] * l)))
-    return qf
+    return em * (math.sinh(m * l) + (h / (m * k)) * math.cosh(m * l)) / (math.cosh(m * l) + (h / (m * k)) * math.sinh(m * l))
 
 
 def effectiveness(qf, h, a_c, dtb):  # finds the single fin effectiveness
-    eff = []
-    for i in range(0, len(qf)):
-        eff.append(qf[i] / (h[i] * a_c * dtb))
-    return eff
+    return qf / (h * a_c * dtb)
 
 
 def efficiency(qf, h, a_f, dtb):
-    eff = []
-    for i in range(0, len(qf)):
-        eff.append(qf[i] / (h[i] * a_f * dtb))
-    return eff
+    return qf / (h * a_f * dtb)
 
 
 def rfin(dtb, qf):
-    r = []
-    for i in range(0, len(qf)):
-        r.append(dtb / qf[i])
-    return r
+    return dtb / qf
 
 
 def rbase(h, a_c):
-    r = []
-    for i in range(0, len(h)):
-        r.append(1/(h[i] * a_c))
-    return r
+    return 1/(h * a_c)
 
 
 def q_tot(n, eta, h, a_f, dtb, a_b):
-    q = []
-    for i in range(0, len(eta)):
-        q.append(n * eta[i] * h[i] * a_f * dtb + h[i] * a_b * dtb)
-    return q
+    return n * eta * h * a_f * dtb + h * a_b * dtb
 
 
 def eta_o(qt, h, a_tot, dtb):
-    e = []
-    for i in range(0, len(qt)):
-        e.append(qt[i] / (h[i] * a_tot * dtb))
-    return e
+    return qt / (h * a_tot * dtb)
 
 
 def roverall(dtb, qt):
-    r = []
-    for i in range(0, len(qt)):
-        r.append(dtb / qt[i])
-    return r
+    return dtb / qt
 
 
 if __name__ == '__main__':
